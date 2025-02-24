851cbfcd89ff39dc6fd196e714d2f28ef7147a27
T-REX-XP <4560084+T-REX-XP@users.noreply.github.com>
2025-02-25 00:54:25 +0100

    Initial commit from your app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import * as React from 'react';
+import { useState, useEffect } from 'react';
 import { mockQuestions } from './questions';
 import QuestionList from './components/QuestionList';
 import NavigationButtons from './components/NavigationButtons';
--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import { Question as QuestionType } from '../types';
 import './Question.css';
 
